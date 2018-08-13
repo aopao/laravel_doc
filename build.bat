7583503256f09fb2c339e583db2d2358f0644255
liuguang <67579722@qq.com>
2018-08-13 22:04:04 +0800

    fix pdf文档章节顺序不正确的问题

--- build.bat
+++ build.bat
@@ -2,12 +2,12 @@
 set header_tpl="<div style=\"padding:12px 0;width:100%;overflow:hidden;border-bottom: 1px solid #eee;color: #5c6163;font-size:14px;\"><div style=\"float:right;\">第_PAGENUM_页</div><div>_SECTION_</div></div>"
 set is_debug=0
 set inputFile=all.html
-set outputFile=laravel_56_doc_zh_cn.pdf
+set outputFile=laravel56_doc_zh_cn.pdf
 if %is_debug% equ 1 (
   set inputFile=debug.html
   set outputFile=debug.pdf
 )
 cd html_files
 echo "%inputFile% ------  %outputFile%" 
-ebook-convert "%inputFile%" "../%outputFile%" -v --paper-size=a4 --level1-toc=//h:h1 --level2-toc=//h:h2 --level3-toc=//h:h3 --authors="流光" --pdf-page-margin-left=18 --pdf-page-margin-right=18 --pdf-header-template=%header_tpl% --pdf-footer-template=%header_tpl%
+ebook-convert "%inputFile%" "../%outputFile%" -v --paper-size=a4 --breadth-first --level1-toc=//h:h1 --level2-toc=//h:h2 --level3-toc=//h:h3 --authors="流光" --pdf-page-margin-left=18 --pdf-page-margin-right=18 --pdf-header-template=%header_tpl% --pdf-footer-template=%header_tpl%
 cd ..
